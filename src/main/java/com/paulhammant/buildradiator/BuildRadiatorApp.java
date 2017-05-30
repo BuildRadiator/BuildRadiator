82ff7051cb8dba669cd260cdb4fcbc769478545e
Paul Hammant <paul@hammant.org>
2017-05-29 20:36:04 -0400

    robots file

--- src/main/java/com/paulhammant/buildradiator/BuildRadiatorApp.java
+++ src/main/java/com/paulhammant/buildradiator/BuildRadiatorApp.java
@@ -52,6 +52,7 @@ public class BuildRadiatorApp extends Jooby {
         // Routes /_ah/start and /_ah/stop - not enabled on Flex containers
 
         assets("/", "index.html");
+        assets("/robots.txt", "robots.txt");
         assets("/moment.min.js", "moment.min.js");
         assets("/moment-duration-format.js", "moment-duration-format.js");
         assets("/vue.min.js", "vue.min.js");
